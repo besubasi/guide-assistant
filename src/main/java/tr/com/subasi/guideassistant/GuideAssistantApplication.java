f2ecdc9404f32e301a07aad53e83c49b820fd9fd
bsubasi <besubasi@gmail.com>
2023-11-02 00:36:22 +0300

    Initial commit

---
+++ src/main/java/tr/com/subasi/guideassistant/GuideAssistantApplication.java
@@ -0,0 +1,13 @@
+package tr.com.subasi.guideassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/com/subasi/guideassistant/GuideAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.subasi.guideassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
